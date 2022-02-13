38d90b23add3e3344405abb55cae739943b3ac68
Mohamad Youness <pixlerfrost@gmail.com>
2022-02-13 14:38:22 -0500

    Fetching Paths for getStaticPaths

--- pages/posts/[postid].js
+++ pages/posts/[postid].js
@@ -12,18 +12,29 @@ function Post({post}) {
 export default Post
 
 export async function getStaticPaths() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await response.json()
+  const paths = data.map(post => {
+    return {
+      params: {
+        postid: `${post.id}`
+      }
+    }
+  })
+
  return {
-  paths: [
-   {
-    params: { postid: '1'}
-   },
-   {
-    params: { postid: '2'}
-   },
-   {
-    params: { postid: '3'}
-   }
-  ],
+  // paths: [
+  //  {
+  //   params: { postid: '1'}
+  //  },
+  //  {
+  //   params: { postid: '2'}
+  //  },
+  //  {
+  //   params: { postid: '3'}
+  //  }
+  // ],
+  paths,
   fallback: false
  }
 }
