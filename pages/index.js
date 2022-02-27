20e0dfb65aecc58a7d9c212c9a5df54cab75bc73
Mohamad Youness <pixlerfrost@gmail.com>
2022-02-26 20:38:17 -0500

    .

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,59 @@
-import {signIn, signOut, useSession} from "next-auth/react"
-import Link from "next/link"
+import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function index() {
- const {session, loading} = useSession()
- 
- return (
-  <>
-   <h1>Home Page</h1>
+  const {session} = useSession()
+  return (
+    <nav className='header'>
+      <h1 className='logo'>
+        <a href='#'>NextAuth</a>
+      </h1>
+        <li>
+          <Link href='/'>
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/dashboard'>
+            <a>Dashboard</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/blog'>
+            <a>Blog</a>
+          </Link>
+        </li>
 
-   {
-    !session && !loading && (
-     <Link href='/api/auth/signin'>
-    <a onClick={e => {
-     e.preventDefault()
-     signIn('github')
-    }}>Sign In</a>
-   </Link>
-    )
-   }
+         {!session} {
 
-    {
-     session && (
-   <Link href='/api/auth/signout'>
-     <a onClick={e => {
-      e.preventDefault()
-      signOut()
-   }}>Sign Out</a>
-   </Link>
-     )
-    }
-
-  <Link href='/api/auth/signout'>
-   <a onClick={e => {
-    e.preventDefault()
-    signOut()
-   }}>Sign Out</a>
-   </Link>
-  </>
- )
+          <li>
+            <Link href='/api/auth/signin'>
+              <a
+                onClick={e => {
+                  e.preventDefault()
+                  signIn('github')
+                }}>
+                Sign In
+              </a>
+            </Link>
+          </li>
+        }
+        
+        {session && (
+          <li>
+            <Link href='/api/auth/signout'>
+              <a
+                onClick={e => {
+                  e.preventDefault()
+                  signOut()
+                }}>
+                Sign Out
+              </a>
+            </Link>
+          </li>
+        )}
+    </nav>
+  )
 }
 
 export default index
