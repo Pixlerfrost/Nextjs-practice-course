7cc00e7d7216d4df05002765e2f2c3294feffeb7
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-01 21:09:24 -0500

    .

--- pages/blog.js
+++ pages/blog.js
@@ -9,13 +9,7 @@ export default Blog
 export async function getServerSideProps(context) {
   const session = await getSession(context)
 
-  if (!session) {
-    return {
-      redirect: {
-        
-      }
-    }
-  }
+
 
   return {
    props: {
