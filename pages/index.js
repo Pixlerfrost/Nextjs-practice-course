7d77a0624d7c35c12c31316d4e6fc3e9ed75bea6
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-05 15:44:17 -0500

    .

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,8 @@
-import Link from "next/link"
-import { useSession, signIn, signOut } from 'next-auth/react';
-
-// using client side session retrieval
 function index() {
-  const { data: session, status } = useSession();
-  console.log(session, status)
-  
- 
-
-  if (status === 'loading') {
-    return <h1>Loading...</h1>;
-  }
-  if (session) {
-    return (
-      <>
-      <Link href="http://localhost:3000/dashboard">
-        <a>dashboard</a>
-      </Link> <br/>   
-        <button type="button" onClick={() => signOut()}>
-          Sign out
-        </button>
-      </>
-    );
-  }
   return (
-    <>
-      Not signed in <br />
-      <button type="button" onClick={() => signIn()}>
-        Sign in
-      </button>
-    </>
-  );
-};
+    <h1>Home Page</h1>
+  )
+}
 
 
 
