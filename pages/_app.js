5e4462259ad019441840b9449449ea67ffc24094
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-05 15:43:15 -0500

    .

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import { SessionProvider } from 'next-auth/react';
-const App = ({ Component, pageProps }) => (
-  <SessionProvider session={pageProps.session}>
-    <Component {...pageProps} />
-  </SessionProvider>
-);
-
-export default App;
